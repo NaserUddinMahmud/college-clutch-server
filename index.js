81e5c0b34b8dc40b2982cbbd0e9d1d93e0396b75
NaserUddinMahmud <mahmudbappy01@gmail.com>
2023-07-24 23:17:48 +0600

    booking data get operation successful

--- index.js
+++ index.js
@@ -44,6 +44,16 @@ async function run() {
     })
 
     // booking
+    app.get('/booking', async(req, res)=>{
+        let query = {};
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }
+      
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+    })
+
     app.post('/booking', async(req, res)=>{
         const booking = req.body;
         const result = await bookingCollection.insertOne(booking);
